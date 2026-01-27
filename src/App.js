ba03159f1f37815d0ec56b8958444c662a98540a
dragon-r <d.rak60@outlook.com>
2026-01-27 13:45:06 +0100

    Included basic global styles
    container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 export function App() {
   return (
-    <>
+    <div className="container">
       <h1>Parcel React App</h1>
       <p>Edit <code>src/App.tsx</code> to get started!</p>
-    </>
+    </div>
   );
 }
