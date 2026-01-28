c74ca1fc871d8594dfb0e299deae75a00cc371f0
dragon-r <d.rak60@outlook.com>
2026-01-28 12:45:14 +0100

    Added Button.js

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './_btn.scss';
+
+function Button({label, type, size, fun, isSubmit}) {
+    let className = "btn" + " btn-" + type + " btn-" + size;    
+
+    const handleClick = (e) => {
+        if (typeof(fun) === "function") {
+            fun();
+        }
+    }
+    return (
+        <button onClick={handleClick} className={className} type={(isSubmit) ? "submit":"button"}>{label}</button>
+    )
+}
+
+export default Button;
