51aa80f295527ebce14498dc687ec8eba1312c59
dragon-r <d.rak60@outlook.com>
2026-01-28 12:46:59 +0100

    Updated Button.js
    Corrected BEM modifier naming

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import './_btn.scss';
 
 function Button({label, type, size, fun, isSubmit}) {
-    let className = "btn" + " btn-" + type + " btn-" + size;    
+    let className = "btn" + " btn--" + type + " btn--" + size;    
 
     const handleClick = (e) => {
         if (typeof(fun) === "function") {
