52dd9e4fb2914e4b00b1ddb55eb2eea5000d1fc0
dragon-r <d.rak60@outlook.com>
2026-01-28 19:46:17 +0100

    Updated Task component
    Made Button render directly in the component. Task passes on its event functions to the Buttons

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,12 +1,25 @@
 import React from "react";
 import './_task.scss';
 
-function Task({task, children}) {
+import Button from "../Button/Button";
+
+function Task({task, onToggle, onDelete}) {
     return (
         <article className={(task.isDone) ? "task task--checked":"task"}>
             <h2 className={(task.isDone) ? "task__title task__title--checked":"task__title"} >{task.title}</h2>
             <div className="task__btns" >
-                {children}
+                <Button 
+                label={(task.isDone) ? "Undone":"Done"} 
+                type={(task.isDone) ? "secondary-alt":"secondary"} 
+                size="medium"
+                fun={() => {onToggle(task)}}
+                />
+                <Button 
+                label="Remove"
+                type="adjacent"
+                size="medium"
+                fun={() => {onDelete(task)}}
+                />
             </div>
         </article>
     )
